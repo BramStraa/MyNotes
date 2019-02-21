e22b4dad0b6e8a78b68335b34535edbbfd2b0c28
Bram Straatman <stijnveld@hotmail.com>
2019-02-21 10:45:53 +0100

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AddNotePage } from '../add-note/add-note';
+import { NoteService } from '../../providers/note-service/note-service';
+import { Note } from '../../models/note.model';
+import { ViewNotePage } from '../view-note/view-note';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +12,35 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public notes: Promise<Note[]>;
+  private note: Note;
+
+
+  constructor(public navCtrl: NavController, public noteService: NoteService) {
 
   }
+
+  ionViewWillEnter() {
+    this.notes = this.getAllNotes();
+
+  }
+
+
+  addNote(){
+    this.navCtrl.push(AddNotePage);
+  }
+
+  getNote(createDate: number) {
+    this.noteService.getNote(createDate).then((n) => {
+      this.note = n;
+      this.navCtrl.push(ViewNotePage, { note: this.note })
+    })
+  }
+
+  getAllNotes(){
+    return this.noteService.getAllNotes();
+  }
+
+
 
 }
